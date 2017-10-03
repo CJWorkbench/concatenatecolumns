85ffa1e3b247329b076d606c9c5773f6df885ce1
alexgonca <alexandre.reportagem@gmail.com>
2017-10-03 14:49:54 -0400

    module which needs user to enter parameters needs to show input unchanged

--- concatenatecolumns.py
+++ concatenatecolumns.py
@@ -10,18 +10,14 @@ class Importable:
     @staticmethod
     def render(wf_module, table):
         newcol = wf_module.get_param_string('newcolumn')
-        cols = wf_module.get_param_string('colnames').split(',')
-        cols = [c.strip() for c in cols]
-
-        if cols == [] or cols == ['']:
-            return table
-
-        for c in cols:
-            if not c in table.columns:
-                wf_module.set_error('There is no column named %s' % c)
-                return None
-
+        if newcol != '':
+            cols = wf_module.get_param_string('colnames').split(',')
+            cols = [c.strip() for c in cols]
+            if cols != [] and cols != ['']:
+                for c in cols:
+                    if not c in table.columns:
+                        wf_module.set_error('There is no column named %s' % c)
+                        return table
+                table[newcol] = table[cols].apply(lambda x: ' '.join([str(i) for i in x if str(i) != 'nan']), axis=1)
         wf_module.set_ready(notify=False)
-
-        table[newcol] = table[cols].apply(lambda x: ' '.join([str(i) for i in x if str(i) != 'nan']), axis=1)
         return table
